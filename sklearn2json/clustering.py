a48fbe594c3b691c4eab1ccd9ee7f4e616acb2a0
csanyig <csanyi.gergely@montana.hu>
2021-04-27 15:40:27 +0200

    duplication

--- sklearn2json/clustering.py
+++ sklearn2json/clustering.py
@@ -59,35 +59,3 @@ def deserialize_k_means(model_dict):
     model._n_threads = model_dict["_n_threads"]
     model._algorithm = model_dict["_algorithm"]
     return model
-
-
-def serialize_k_means(model):
-    serialized_model = {
-        'meta': 'kmeans',
-        "cluster_centers_": model.cluster_centers_.tolist(),
-        "labels_": model.labels_.tolist(),
-        "n_features_in_": model.n_features_in_,
-        "_n_init": model._n_init,
-        "_tol": model._tol,
-        "_n_threads": model._n_threads,
-        "inertia_": model.inertia_,
-        "n_iter_": model.n_iter_,
-        "_algorithm": model._algorithm,
-        'params': model.get_params()
-    }
-
-    return serialized_model
-
-
-def deserialize_k_means(model_dict):
-    model = KMeans(**model_dict['params'])
-    model.cluster_centers_ = np.array(model_dict["cluster_centers_"])
-    model.labels_ = np.array(model_dict["labels_"])
-    model.inertia_ = model_dict["inertia_"]
-    model.n_iter_ = model_dict["n_iter_"]
-    model.n_features_in_ = model_dict["n_features_in_"]
-    model._n_init = model_dict["_n_init"]
-    model._tol = np.float64(model_dict["_tol"])
-    model._n_threads = model_dict["_n_threads"]
-    model._algorithm = model_dict["_algorithm"]
-    return model
