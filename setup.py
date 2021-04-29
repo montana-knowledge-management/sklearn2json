e61e3eb7a44be60031e3771512a5d7f5de1f2422
Tamas Orosz <oroszt@montana.hu>
2021-04-29 14:33:46 +0200

    setup.py and setup.cfg set

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+setup(
+    name="Sklearn2JSON",
+    install_requires=[
+        "scikit-learn",
+        "numpy",
+        "scipy"
+
+    ]
+)
